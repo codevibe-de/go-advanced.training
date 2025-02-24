edc978ee5a6e1acce83de701a1999fdb85456f0b
Thomas Auinger <thomas@auinger.de>
2025-02-24 07:58:57 +0100

    132 grpc

---
+++ tmp/132-grpc/cmd/server/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"google.golang.org/grpc"
+	"local/132-grpc/pizza/pb"
+	"log"
+	"net"
+)
+
+type server struct {
+	pb.UnimplementedPizzaServiceServer
+}
+
+// implement the interface PizzaServiceServer:
+
+func (s *server) GetProduct(ctx context.Context, in *pb.ProductRequest) (*pb.ProductResponse, error) {
+	return &pb.ProductResponse{
+		ProductId: in.ProductId,
+		Name:      "Pizza",
+		Price:     7.99,
+	}, nil
+}
+
+func (s *server) ListProducts(in *pb.ProductListRequest, stream pb.PizzaService_ListProductsServer) error {
+	return nil
+}
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterPizzaServiceServer(s, &server{})
+	log.Printf("Server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
